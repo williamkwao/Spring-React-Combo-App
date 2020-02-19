19b9929fb837a8f21dc76f3dca8260a2389035fb
E <aninyeen@vcu.edu>
2020-02-18 21:09:48 -0500

    Added source files

---
+++ src/main/java/e/the/awesome/springreactcomboapp/SpringReactComboAppApplication.java
@@ -0,0 +1,13 @@
+package e.the.awesome.springreactcomboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactComboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactComboAppApplication.class, args);
+	}
+
+}

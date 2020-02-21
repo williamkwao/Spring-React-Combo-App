b7b5f5a8b4620d7e5c8754cab0a3ce85a2c77fa1
Emmanuella Aninye - e0a009j <Emmanuella.Aninye@walmart.com>
2020-02-21 14:45:13 -0500

    Server initialization

--- src/main/java/e/the/awesome/springreactcomboapp/SpringReactComboAppApplication.java
+++ src/main/java/e/the/awesome/springreactcomboapp/SpringReactComboAppApplication.java
@@ -2,13 +2,20 @@ package e.the.awesome.springreactcomboapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringReactComboAppApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// Customize the application or call application.sources(...) to add sources
+		// Since our example is itself a @Configuration class (via @SpringBootApplication)
+		// we actually don't need to override this method.
+		return application;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactComboAppApplication.class, args);
 	}
-
 }
